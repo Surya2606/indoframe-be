d8433351a2cf107c35dca12112b099323f0edbce
Surya2606 <suryahermawan26@gmail.com>
2026-08-16 16:21:51 +0700

    fix: adjust file articleRoutes.js

--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 // Mengambil dari folder controllers yang ada di dalam src/
 const articleController = require("../controllers/articleController");
 
+// Route GET (Ambil semua artikel)
 router.get("/", (req, res, next) => articleController.getAllArticles(req, res, next));
+
+// Route POST (Simpan artikel baru) -- TAMBAHKAN BARIS INI!
+router.post("/", (req, res, next) => articleController.createArticle(req, res, next));
 
 module.exports = router;
